173ba271022b1aa78298e51ff4073bb2bcecdf84
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2023-02-16 15:52:25 +0300

    SCP106cntm005
    PRE-RELEASE!
    Основные функции добавлены, после теста и исправления багов будет выпущена финальная версия

--- SCP106-Contaiment/SCP106-Contaiment/Config.cs
+++ SCP106-Contaiment/SCP106-Contaiment/Config.cs
@@ -14,8 +14,17 @@ namespace SCP106_Contaiment
         [Description("Список фраз касье")]
         public List<string> cassie { get; set; } = new List<string>()
         {
-            "WARNING . DETECTED ATTEMPT OF ACTIVATING SCP 1 0 6 CONTAINMENT . PLEASE PUT BIOLOGICAL ORGANISM IN CONTAIN FACILITY .G2 AND RUN PROTOCOLS OF CONTAINMENT",
+            "WARNING . DETECTED ATTEMPT OF ACTIVATING SCP 1 0 6 CONTAINMENT . STARTING ELECTROMAGNETISM DEVICE FOR CONTAINING . LEFT 90 SECONDS",
             "BIOLOGICAL ORGANISM DETECTED . SCP 1 0 6 CONTAINMENT READY TO START . CONTAMINATION START IN 3 . 2 . 1",
+            "WARNING . DEVICE STARTING COMPLETED SUCCESSFULLY . PLEASE PUT BIOLOGICAL ORGANISM IN CONTAIN FACILITY .G2 AND RUN PROTOCOLS OF CONTAINMENT"
         };
+        public List<string> hints { get; set; } = new List<string>() 
+        {
+            "Необходимо активировать все генераторы",
+            "Нажмите L CTRL для подготовки условия содержания",
+            "Нажмите L CTRL для запуска условия содержания",
+            "Вы не можете запустить условие содержания так как в комнате нет биологического объекта"
+        };
+        
     }
 }
--- SCP106-Contaiment/SCP106-Contaiment/EventHandlercs.cs
+++ SCP106-Contaiment/SCP106-Contaiment/EventHandlercs.cs
@@ -4,14 +4,25 @@ using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 using PlayerRoles;
+using MEC;
+using Exiled.API.Enums;
 
 namespace SCP106_Contaiment
 {
     public class EventHandlers : Contaiment
     {
+        public bool first_start = false;
         public void OnWaitingForPlayers()
         {
             InjectPositions();
+        }
+        private bool IsGeneratesActivated()
+        {
+            int count = 0;
+            IEnumerator<Generator> e = Generator.List.GetEnumerator();
+            while (e.MoveNext()) { if (e.Current.IsEngaged == true) count++; else continue; }                          
+            if (count == 3) return true;
+            else return false;
         }
 
         public void OnProccesingHotKey(ProcessingHotkeyEventArgs ev)
@@ -19,7 +30,6 @@ namespace SCP106_Contaiment
             Player _object = null;
             Player scp_106 = null;
             bool get_pos = false;
-            bool first_start = false;
             GetPos(ev.Player, ref get_pos);
             if (get_pos != true)
                 return;
@@ -28,7 +38,7 @@ namespace SCP106_Contaiment
                 OnEnabled(ev.Player);
                 first_start = true;
             }
-            if (ev.Hotkey == HotkeyButton.Keycard && ev.Player.CurrentRoom.Type == RoomType.Hcz106 && ev.Player.Role.Side != Side.Scp && fisrt_start != false)
+            if (ev.Hotkey == HotkeyButton.Keycard && ev.Player.CurrentRoom.Type == RoomType.Hcz106 && ev.Player.Role.Side != Side.Scp && first_start != false)
             {
                 IEnumerator<Player> p_list = Player.List.GetEnumerator();
                 while (p_list.MoveNext())
@@ -54,6 +64,45 @@ namespace SCP106_Contaiment
                         }
                         StartingContaiment(ev.Player, _object, scp_106);                       
                     }
+                }
+            }            
+        }
+        public void OnChangingMoveState(ChangingMoveStateEventArgs ev)
+        {
+            bool get_pos = false;
+            GetPos(ev.Player, ref get_pos);
+            if (get_pos != true)
+            {
+                if (Timing.CurrentCoroutine != null) Timing.KillCoroutines();
+                return;
+            }               
+            Timing.RunCoroutine(ContaimentMenuHint(ev.Player, ev.Player.CurrentRoom));
+        }
+
+        private IEnumerator<float> ContaimentMenuHint(Player player, Room room) 
+        {
+            for (; ; )
+            {
+                int count = 0;
+                IEnumerator<Player> __enum = room.Players.GetEnumerator();
+                while (__enum.MoveNext())
+                    count++;
+                yield return Timing.WaitForSeconds(1f);
+                if (IsGeneratesActivated() == false)
+                {
+                    player.ShowHint(Plugin.Instance.Config.hints[1]);
+                }
+                if (first_start == true && count < 2 && IsGeneratesActivated() != true)
+                {
+                    player.ShowHint(Plugin.Instance.Config.hints[4]);
+                }
+                if (first_start != false && IsGeneratesActivated() != true)
+                {
+                    player.ShowHint(Plugin.Instance.Config.hints[3]);
+                }
+                if (first_start == false && IsGeneratesActivated() != true)
+                {
+                    player.ShowHint(Plugin.Instance.Config.hints[2]);
                 }
             }
         }
